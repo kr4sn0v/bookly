92a62913df6a350c8421e26078f18d6cc4545a86
Igor Krasnov <qiuscou@icloud.com>
2025-10-24 16:03:35 +0500

    The basic structure of the project has been created

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import css from 'rollup-plugin-import-css';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'src/app.js',
-  output: {
-    dir: 'dist',
-    format: 'iife',
-  },
-  plugins: [css({ output: 'bundle.css' }), nodeResolve()],
+    input: 'src/app.js',
+    output: {
+        dir: 'dist',
+        format: 'iife',
+    },
+    plugins: [css({ output: 'bundle.css' }), nodeResolve()],
 };
